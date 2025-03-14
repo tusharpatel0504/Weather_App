d90deec5b1beadede8966c19cd8d5928dfe9c69f
TUSHAR <tushar789patel@gmail.com>
2025-03-14 23:43:22 +0530

    changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const search = document.querySelector(".search input");
 const btn = document.querySelector(".search button");
+const weather_icon = document.querySelector(".weather img");
      
     async function getWeatherData(city) {
     const response = await fetch(apiUrl + city + `&appid=${apnikey}`);
@@ -13,9 +14,29 @@ const btn = document.querySelector(".search button");
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
+    if(data.weather[0].main == "Clouds"){
+        weather_icon.src = "clouds.png";
+    }
+    else if(data.weather[0].main == 'Clear'){
+        weather_icon.src = "clear.png";
+    }
+    else if(data.weather[0].main == 'Rain'){
+        weather_icon.src = "rain.png";
+    }
+    else if(data.weather[0].main == 'Drizzle'){
+        weather_icon.src = "drizzle.png";
+    }
+    else if(data.weather[0].main == 'Haze'){
+        weather_icon.src = "mist.png";
+    }
 }
 btn.addEventListener("click", () => {
     getWeatherData(search.value);
 })
+search.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        getWeatherData(search.value);
+    }
+});
